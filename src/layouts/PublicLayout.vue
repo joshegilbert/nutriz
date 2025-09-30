f38b03bc82da9c55318f9249bee99f0842072a92
jgilb811 <jgilb811@gmail.com>
2025-09-30 23:02:59 +0000

    updating the enviorment

---
+++ src/layouts/PublicLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-main class="d-flex justify-center align-center bg-grey-lighten-4">
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+// This is a simple layout for public-facing pages like login or home.
+</script>
